7b0a8ab775a0f4894ca82da7d13a6018310ef375
81784244368 <paulo.silva-junior@serpro.gov.br>
2025-01-02 16:20:12 -0300

    alteração para favorecer layout mobile

--- app/cabecalho.tsx
+++ app/cabecalho.tsx
@@ -1,33 +1,32 @@
-import { Button, Text, View } from "react-native";
 import React, { PropsWithChildren } from "react";
+import {Text, View} from "react-native";
 
 type MesCabecalhoProps = PropsWithChildren<{
   texto: string;
-  // values: string[];
-  // selectedValue: string;
-  // setSelectedValue: (value: string) => void;
+  mes: any[];
 }>;
-export default function MesCabecalho({texto, children}: MesCabecalhoProps) {
+export default function MesCabecalho({texto, mes}: MesCabecalhoProps) {
    return(
     <>
         <Text style={{textAlign: "center"}}>{texto}</Text>
         <View style={{
           flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: 8,
-    backgroundColor: 'aliceblue',
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          marginTop: 15,
         }}>
-            <View style={{width: 100, height: 50, backgroundColor: 'powderblue'}}><Text>Dia</Text></View>
-            <View style={{width: 100, height: 50, backgroundColor: 'powderblue'}}><Text>&nbsp;</Text></View>
-            <View style={{width: 200, height: 50, backgroundColor: 'powderblue'}}><Text>Livro, Capítulos e Versículos</Text></View>
-            <View style={{width: 100, height: 50, backgroundColor: 'powderblue'}}><Text>&nbsp;</Text></View>
-            <View style={{width: 100, height: 50, backgroundColor: 'powderblue'}}><Text>Livro EP</Text></View>
-            <View style={{width: 100, height: 50, backgroundColor: 'powderblue'}}><Text>Capítulo</Text></View>
-            <View style={{width: 200, height: 50, backgroundColor: 'powderblue'}}><Text>Espírito de Profecia</Text></View>
+          {mes.map(valor => 
+            <View style={{width: 50, 
+            height: 50, backgroundColor: "red", 
+            marginTop: 8, alignContent: "center", 
+            alignItems: "center", 
+            justifyContent: "center"}}>
+              <Text style={{fontSize: 16, textAlign: "center"}}>
+                {valor.dia}
+                </Text>
+            </View>  )}
         </View>
    
-
-        {children}
          
     </>
    );
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,66 @@
-import Linha from "./linha";
+import { ScrollView, Text, View } from "react-native";
+
 import MesCabecalho from "./cabecalho";
-import { View } from "react-native";
+import React from "react";
 
 export default function Index() {
+    const janeiro = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30},{dia: 31}];
+    const fevereiro = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28}];
+    const marco = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30},{dia: 31}];
+    const abril = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30}];
+    const maio = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30},{dia: 31}];
+    const junho = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30}];
+    const julho = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30},{dia: 31}];
+    const agosto = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30},{dia: 31}];
+    const setembro = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30}];
+    const outubro = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30},{dia: 31}];
+    const novembro = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30}];
+    const dezembro = [{dia: 1},{dia: 2},{dia: 3},{dia: 4},{dia: 5},{dia: 6},{dia: 7},{dia: 8},{dia: 9},{dia: 10},{dia: 11},{dia: 12},{dia: 13},{dia: 14},{dia: 15},{dia: 16},{dia: 17},{dia: 18},{dia: 19},{dia: 20},{dia: 21},{dia: 22},{dia: 23},{dia: 24},{dia: 25},{dia: 26},{dia: 27},{dia: 28},{dia: 29},{dia: 30},{dia: 31}];
+  
   return (
-    <View style={{padding: 10, flex: 1}}>
-            <MesCabecalho texto="JANEIRO">
-            <Linha dia="01" capBiblia="Salmos 1 a 2" sigla="PP" capEspirito="--" nomEspirito="Prefácio"/>
-            <Linha dia="02" capBiblia="Salmos 4 a 6" sigla="PP" capEspirito="Cap. 01" nomEspirito="Porque foi permitido o pecado?"/>
-            </MesCabecalho>
+    <>
+    <View>
+    <Text style={{textAlign: 'center', fontSize: 24}}>Plano de Estudo Bíblico</Text>
+    <Text style={{textAlign: 'center', fontSize: 24}}>Com Espírito de Profecia</Text>
     </View>
+    <ScrollView>
+      <View style={{ padding: 15 }}>
+        <MesCabecalho texto="JANEIRO" mes={janeiro} />
+      </View>
+      <View style={{ padding: 18 }}>
+        <MesCabecalho texto="FEVEREIRO" mes={fevereiro} />
+      </View>
+      <View style={{ padding: 15 }}>
+        <MesCabecalho texto="MARÇO" mes={marco} />
+      </View>
+      <View style={{ padding: 18 }}>
+        <MesCabecalho texto="ABRIL" mes={abril} />
+      </View>
+      <View style={{ padding: 15 }}>
+        <MesCabecalho texto="MAIO" mes={maio} />
+      </View>
+      <View style={{ padding: 18 }}>
+        <MesCabecalho texto="JUNHO" mes={junho} />
+      </View>
+      <View style={{ padding: 15 }}>
+        <MesCabecalho texto="JULHO" mes={julho} />
+      </View>
+      <View style={{ padding: 18 }}>
+        <MesCabecalho texto="AGOSTO" mes={agosto} />
+      </View>
+      <View style={{ padding: 18 }}>
+        <MesCabecalho texto="SETEMBRO" mes={setembro} />
+      </View>
+      <View style={{ padding: 15 }}>
+        <MesCabecalho texto="OUTUBRO" mes={outubro} />
+      </View>
+      <View style={{ padding: 18 }}>
+        <MesCabecalho texto="NOVEMBRO" mes={novembro} />
+      </View>
+      <View style={{ padding: 15 }}>
+        <MesCabecalho texto="DEZEMBRO" mes={dezembro} />
+      </View>
+    </ScrollView>
+    </>
   );
 }
