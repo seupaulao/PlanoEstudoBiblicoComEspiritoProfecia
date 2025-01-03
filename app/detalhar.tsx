5c9fc5971e21eef7a3d82332969c7407860b9d2a
81784244368 <paulo.silva-junior@serpro.gov.br>
2025-01-03 14:39:05 -0300

    correção visual do detalhar mais states

--- app/detalhar.tsx
+++ app/detalhar.tsx
@@ -1,6 +1,8 @@
 import { Link, useLocalSearchParams, useNavigation, useRootNavigationState, useRouter } from 'expo-router';
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View, } from "react-native";
 import { getCapitulosBibliaPlano, getCapitulosESPlano, getNomeLivro, getSiglaESPlano, getTituloCapituloESPlano } from "./planos";
+
+import { useState } from 'react';
 
 //TODO usar mais Views - Melhorar apresentação dessa tela
 //TODO substituir buttons por touchables ou Pressables [recomendado] - melhor ainda: faça seu proprio button
@@ -8,6 +10,8 @@ import { getCapitulosBibliaPlano, getCapitulosESPlano, getNomeLivro, getSiglaESP
 
 export default function DetalharScreen() {
   //const router = useRouter();
+  const [leuBiblia, setLeuBiblia] = useState(0);
+  const [leuEP, setLeuEP] = useState(0);
   const navegacao = useNavigation();
   const params = useLocalSearchParams();
     const mes = params.mes;
@@ -16,14 +20,20 @@ export default function DetalharScreen() {
       return mes+"_"+dia;
     }
     return (
-        <View style={{ flex: 1 }}>
-           <Text style={{textAlign: 'center'}}>Dia de estudo: {dia}/{mes}</Text>
-          <Text style={{textAlign: 'center'}}>Texto Biblico: {getCapitulosBibliaPlano( getChave() )}</Text>
-          <Pressable style={styles.botaoazul} onPress={() => {}} ><Text style={styles.fontebotao}>Realizou o Estudo da Bíblia</Text></Pressable> 
-          <Text style={{textAlign: 'center'}}>Capitulos Espirito Profecia: {getCapitulosESPlano( getChave() )}</Text> 
-          <Text style={{textAlign: 'center'}}>Livro Espirito Profecia: {getNomeLivro( getSiglaESPlano( getChave() ))}</Text> 
-          <Text style={{textAlign: 'center'}}>Titulo Capitulo Espirito Profecia: {getTituloCapituloESPlano( getChave() )}</Text> 
-          <Pressable style={styles.botaoverde} onPress={() => {}} ><Text style={styles.fontebotao}>Realizou a Leitura do espírito de profecia</Text></Pressable>
+        <View style={{ margin: 10, flexWrap: 'wrap', alignContent: 'center', }}>
+          <View style={styles.blocoLeitura}>
+              <Text style={styles.fontDescricaoTitulo}>Bíblia </Text>
+              <Text style={styles.fontDescricao}>Dia de estudo: {dia}/{mes}</Text>
+              <Text style={styles.fontDescricao}>Texto Biblico: {getCapitulosBibliaPlano( getChave() )}</Text>
+          </View>
+          <Pressable style={leuBiblia == 0 ? styles.botaoazul : styles.botaoverde} onPress={() => {setLeuBiblia(1);}} ><Text style={styles.fontebotao}>{leuBiblia == 0 ? "Realizou o Estudo da Bíblia": "Lido!"}</Text></Pressable> 
+          <View style={styles.blocoLeitura}>
+              <Text style={styles.fontDescricaoTitulo}>Espirito de Profecia </Text>
+              <Text style={styles.fontDescricao}>Capitulo: {getCapitulosESPlano( getChave() )}</Text> 
+              <Text style={styles.fontDescricao}>Livro: {getNomeLivro( getSiglaESPlano( getChave() ))}</Text> 
+              <Text style={styles.fontDescricao}>Titulo: {getTituloCapituloESPlano( getChave() )}</Text> 
+          </View>
+          <Pressable style={leuEP == 0 ? styles.botaoazul : styles.botaoverde} onPress={() => {setLeuEP(1);}} ><Text style={styles.fontebotao}>{leuEP == 0 ? "Realizou a Leitura do Espírito de Profecia": "Lido!"}</Text></Pressable>
           <Pressable style={styles.botaoazulforte} onPress={() => {navegacao.goBack()}} ><Text style={styles.fontebotaobranco}>Voltar</Text></Pressable>
         </View>
       );
@@ -31,6 +41,15 @@ export default function DetalharScreen() {
 
 
 const styles = StyleSheet.create({
+  fontDescricaoTitulo: {
+    textAlign: 'center' , fontSize: 24, marginBlockEnd: 20
+  },
+  fontDescricao: {
+    textAlign: 'left' , fontSize: 18, marginBlockStart: 10
+  },
+  blocoLeitura: {
+    marginTop: 15
+  }, 
   fontebotao: {
       fontSize: 16,
       textAlign: 'center'
@@ -43,7 +62,9 @@ const styles = StyleSheet.create({
   botaoazul: {
     backgroundColor: '#0000cc35',
     color: '#ffffff',
-    padding: 15
+    padding: 15,
+    marginTop: 50
+    
   },
   botaoazulforte: {
     backgroundColor: '#0000cccc',
@@ -51,12 +72,15 @@ const styles = StyleSheet.create({
     padding: 15,
     justifyContent: 'center',
     alignItems: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginTop: 10
   },
   botaoverde: {
     backgroundColor: '#00bb0035',
     color: '#ffffff',
-    padding: 15
+    padding: 15,
+    marginTop: 50
   }
 
 });
+
